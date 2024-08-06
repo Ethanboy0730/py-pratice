f7879795f04e8be128a9f66703f8a221460612c0
Ethanboy0730 <160888843+Ethanboy0730@users.noreply.github.com>
2024-08-06 16:16:31 +0800

    Add files via upload

--- gui.py
+++ gui.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 import numpy as np
 from joblib import load
-from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
 # 加載模型
 model = load('lr_model.joblib')
@@ -13,16 +12,10 @@ def predict():
         input_values = [float(entry.get()) for entry in entries]
         test = np.array(input_values).reshape(1, -1)
         case = model.predict(test)
-        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-        mape = mean_absolute_percentage_error(y_test, y_pred)
+        
+        price_prediction = f"輸入這些條件後，預測成交價格為 {case.item():.2f} 萬"
 
-        price_prediction = f"開價1600萬，成交價預測{case.item():.2f}萬，折價率預測為{(1 - case.item() / 1600) * 100:.2f}%"
-        price_error_range = f"價格誤差範圍介於{(case.item() - rmse):.2f}萬，{(case.item() + rmse):.2f}萬"
-        discount_rate_range = f"折價率誤差範圍介於{(1 - (case.item() - rmse) / 1600) * 100:.2f}%，{(1 - (case.item() + rmse) / 1600) * 100:.2f}%"
-        discount_rate_error = "折價率誤差正負2.92%"
-        avg_price_error = f"預測價格實際價格平均會差{round(mape * 100, 2)}%"
-
-        result_label.config(text=f"{price_prediction}\n{price_error_range}\n{discount_rate_range}\n{discount_rate_error}\n{avg_price_error}")
+        result_label.config(text=price_prediction)
     except Exception as e:
         result_label.config(text=f"錯誤: {e}")
 
@@ -53,3 +46,5 @@ result_label.pack(pady=10)
 
 # 運行 GUI
 root.mainloop()
+
+
